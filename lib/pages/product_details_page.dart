a1643f7a7e3f13458cc1b9eea3d0cea0e253cad6
joceesagar <joshisagarm2@gmail.com>
2024-12-16 22:11:17 +0545

    added final layout to the app

--- lib/pages/product_details_page.dart
+++ lib/pages/product_details_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shopping_app/cart_provider.dart';
+import 'package:shopping_app/providers/cart_provider.dart';
 
 class ProductDetailsPage extends StatefulWidget {
   final Map<String, Object> product;
@@ -65,7 +65,9 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
           const Spacer(),
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Image.asset(widget.product['imageUrl'] as String),
+            child: Image.asset(widget.product['imageUrl'] as String,
+            height: 250,
+            ),
           ),
           const Spacer(
             flex: 2,
@@ -117,7 +119,7 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
                     onPressed: onTap,
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Theme.of(context).colorScheme.primary,
-                      minimumSize: const Size(double.infinity, 50),
+                      fixedSize: const Size(350, 50),
                     ),
                     icon: const Icon(Icons.shopping_cart_outlined,
                         color: Colors.black), // Add an icon here
